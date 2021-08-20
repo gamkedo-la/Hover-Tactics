c73b0c9cf9b5a753052076e680911b2f3213f05e
tiagosomda <tsomda@gmail.com>
2021-08-20 19:10:26 -0400

    added waypoint gizmos for better dev visualization

--- Assets/Scripts/Gameplay/WayPoints.cs
+++ Assets/Scripts/Gameplay/WayPoints.cs
@@ -26,4 +26,18 @@ public class WayPoints : MonoBehaviour
 
         return WayPointArray[index % WayPointArray.Length];
     }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+
+        for(int i = 0; i < WayPointArray.Length; i++)
+        {
+            if(i > 0)
+            {
+                Gizmos.DrawLine(WayPointArray[i-1].position, WayPointArray[i].position);
+            }
+            Gizmos.DrawSphere(WayPointArray[i].position, 1f);
+        }
+    }
 }
