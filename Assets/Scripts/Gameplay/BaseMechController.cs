501d6e95dd83afccd47a7f4cf65d01460c5b7336
tiagosomda <tsomda@gmail.com>
2021-08-20 15:43:21 -0400

    created BaseMechController to split input and mech controller

---
+++ Assets/Scripts/Gameplay/BaseMechController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class BaseMechController : MonoBehaviour
+{
+    [Header("Speeds")]
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float backwardSpeed;
+    [SerializeField] private float sideSpeed;
+    [Header("Turning")]
+    [SerializeField] protected HoverMechAnimation hoverMechAnimation;
+    [SerializeField] private float turnSpeed;
+    [SerializeField] protected float turnSensitivity;
+    [Space]
+    [SerializeField] protected GameObject Cursor;
+
+    private Rigidbody rb;
+    protected MechBoost mechBoost;
+
+    protected float horizontal;
+    protected float vertical;
+    protected float turnDir;
+
+    public void StopMovement()
+    {
+        rb.velocity = Vector3.zero;
+    }
+
+    public void Teleport(float value)
+    {
+        StopMovement();
+        Vector3 position = transform.position;
+        ObjectPooler.instance.SpawnFromPool("TeleportParticles", position, Quaternion.identity);
+        float y = position.y;
+        position += hoverMechAnimation.transform.forward * (value * 2.0f);
+        position.y = y;
+        transform.position = position;
+        ObjectPooler.instance.SpawnFromPool("TeleportParticles", position, Quaternion.identity);
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mechBoost = GetComponent<MechBoost>();
+
+        Assert.IsNotNull(rb, "Rigidbody is null!");
+        Assert.IsNotNull(mechBoost, "Mech Boost is null!");
+    }
+    
+    private void Update()
+    {
+        SetInput();
+        Turn();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    protected virtual void SetInput()
+    {
+        // empty on purpose, child classes need to implement it.
+    }
+
+
+    private void Move()
+    {
+        Vector3 rightDirection = hoverMechAnimation.transform.right;
+        rightDirection.y = 0.0f;
+        Vector3 movement =
+            (hoverMechAnimation.transform.forward * vertical * (vertical > 0 ? forwardSpeed : backwardSpeed)) +
+            (rightDirection * horizontal * sideSpeed);
+        
+        rb.velocity = (mechBoost == null) ? movement : movement * mechBoost.GetBoostValue();
+    }
+
+    private void Turn()
+    {
+        hoverMechAnimation.SetYRotation(hoverMechAnimation.GetYRotation() + turnDir * turnSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/MechController.cs
+++ Assets/Scripts/MechController.cs
@@ -1,62 +1,11 @@
 ﻿using UnityEngine;
-using UnityEngine.Assertions;
 
-public class MechController : MonoBehaviour
+public class MechController : BaseMechController
 {
-    [Header("Speeds")]
-    [SerializeField] float forwardSpeed;
-    [SerializeField] float backwardSpeed;
-    [SerializeField] float sideSpeed;
-    [Header("Turning")]
-    [SerializeField] HoverMechAnimation hoverMechAnimation;
-    [SerializeField] float turnSpeed;
-    [SerializeField] float turnSensitivity;
-    [Space]
-    [SerializeField] GameObject Cursor;
-
-    private Rigidbody rb;
-    private MechBoost mechBoost;
-
-    private float horizontal;
-    private float vertical;
-    private float turnDir;
-
-    public void StopMovement()
-    {
-        rb.velocity = Vector3.zero;
-    }
-
-    public void Teleport(float value)
-    {
-        StopMovement();
-        Vector3 position = transform.position;
-        ObjectPooler.instance.SpawnFromPool("TeleportParticles", position, Quaternion.identity);
-        float y = position.y;
-        position += hoverMechAnimation.transform.forward * (value * 2.0f);
-        position.y = y;
-        transform.position = position;
-        ObjectPooler.instance.SpawnFromPool("TeleportParticles", position, Quaternion.identity);
-    }
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        mechBoost = GetComponent<MechBoost>();
-
-        Assert.IsNotNull(rb, "Rigidbody is null!");
-        Assert.IsNotNull(mechBoost, "Mech Boost is null!");
-    }
-    
-    private void Update()
+    protected override void SetInput()
     {
         HandleMovementInput();
         HandleMouseInput();
-        Turn();
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
     }
 
     private void HandleMovementInput()
@@ -76,21 +25,5 @@ public class MechController : MonoBehaviour
         Vector3 relativeDirection = hoverMechAnimation.transform.InverseTransformPoint(targetPoint);
         turnDir = Mathf.Atan2(relativeDirection.x, relativeDirection.z) * Mathf.Rad2Deg * turnSensitivity;
         turnDir = Mathf.Clamp(turnDir, -1, 1);
-    }
-
-    private void Move()
-    {
-        Vector3 rightDirection = hoverMechAnimation.transform.right;
-        rightDirection.y = 0.0f;
-        Vector3 movement =
-            (hoverMechAnimation.transform.forward * vertical * (vertical > 0 ? forwardSpeed : backwardSpeed)) +
-            (rightDirection * horizontal * sideSpeed);
-        
-        rb.velocity = (mechBoost == null) ? movement : movement * mechBoost.GetBoostValue();
-    }
-
-    private void Turn()
-    {
-        hoverMechAnimation.SetYRotation(hoverMechAnimation.GetYRotation() + turnDir * turnSpeed * Time.deltaTime);
     }
 }
