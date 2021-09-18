e31accbbece4ece06550ac0e8541825a5b2bcb58
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2021-09-18 21:46:29 +0500

    Improved Enemy Mech AI: Now, enemy will move towards player mech when close and goes patrolling when player mech gets far. Layer Bug Fixes.

--- Assets/Scripts/Gameplay/Enemies/EnemyFollowWayPoints.cs
+++ Assets/Scripts/Gameplay/Enemies/EnemyFollowWayPoints.cs
@@ -3,23 +3,53 @@ using UnityEngine;
 [RequireComponent(typeof(EnemyMechController))]
 public class EnemyFollowWayPoints : MonoBehaviour
 {
-
+    public string targetTag = "Player";
+    public float targetDetectDistance = 30.0f;
+    [Space]
     public WayPoints wayPoints;
     public float minDistanceToTarget = 1f;
+
     private int currentWayPointIndex = 0;
     private EnemyMechController enemyMechController;
+    private GameObject[] targetObjects;
+    private int targetStatus = -1;
 
     private void Start()
     {
         enemyMechController = GetComponent<EnemyMechController>();
+        targetObjects = GameObject.FindGameObjectsWithTag(targetTag);
     }
 
     private void Update()
     {
-        if(enemyMechController.IsNearTarget())
+        bool gotTarget = false;
+        for(int i = 0; i < targetObjects.Length; i++)
         {
-            currentWayPointIndex = wayPoints.GetNextIndex(currentWayPointIndex);
-            enemyMechController.SetTarget(wayPoints.GetWayPointAt(currentWayPointIndex));
+            if(Vector3.Distance(
+                targetObjects[i].transform.position,
+                wayPoints.GetWayPointAt(currentWayPointIndex).position)
+            <= targetDetectDistance)
+            {
+                enemyMechController.SetTarget(targetObjects[i].transform);
+                targetStatus = 1;
+                gotTarget = true;
+                break;
+            }
+        }
+        if(targetStatus == 1 && !gotTarget) targetStatus = -1;
+
+        if(targetStatus != 1)
+        {
+            if(targetStatus == -1)
+            {
+                enemyMechController.SetTarget(wayPoints.GetWayPointAt(currentWayPointIndex));
+                targetStatus = 0;
+            }
+            else if(enemyMechController.IsNearTarget())
+            {
+                currentWayPointIndex = wayPoints.GetNextIndex(currentWayPointIndex);
+                enemyMechController.SetTarget(wayPoints.GetWayPointAt(currentWayPointIndex));
+            }
         }
     }
 
